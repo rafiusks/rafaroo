f004d90c6ba2a8b22c466de96b9a22ba7b204857
Rafael Vidal <rafael@oqea.com>
2022-10-03 22:41:31 +0800

    top nav layout

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,14 @@
 import { Text, View } from "react-native"
 import React, { useLayoutEffect } from "react"
 import { useNavigation } from "@react-navigation/native"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { Image } from "react-native"
+import {
+	UserIcon,
+	ChevronDownIcon,
+	SearchIcon,
+	AdjustmentsIcon,
+} from "react-native-heroicons/outline"
 
 function HomeScreen() {
 	const navigation = useNavigation()
@@ -12,9 +20,23 @@ function HomeScreen() {
 	}, [])
 
 	return (
-		<View>
-			<Text className="text-red-500">Home Screen</Text>
-		</View>
+		<SafeAreaView className="bg-white pt-5">
+			{/* Header */}
+			<View className="flex-row pb-3 items-center mx-4 space-x-2">
+				<Image
+					source={{ uri: "https://links.papareact.com/wru" }}
+					className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+				/>
+				<View className="flex-1">
+					<Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+					<Text className="font-bold text-xl">
+						Current Location
+						<ChevronDownIcon size={20} color="#00CCBB" />
+					</Text>
+				</View>
+				<UserIcon size={35} color="#00CCBB" />
+			</View>
+		</SafeAreaView>
 	)
 }
 
