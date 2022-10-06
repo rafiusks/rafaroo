a9f56632ac5fcb9c1899cbbc0fa8cb24d834b771
Rafael Vidal <rafael@oqea.com>
2022-10-06 23:02:44 +0800

    initial menu items work

---
+++ components/DishRow.js
@@ -0,0 +1,62 @@
+import { View, Text, TouchableOpacity, Image } from "react-native"
+import React, { useState } from "react"
+import ReactCurrencyFormatter from "react-currency-formatter-v2"
+import { urlFor } from "../sanity"
+import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid"
+
+const DishRow = ({ id, name, description, price, image }) => {
+	const [isPressed, setIsPressed] = useState(false)
+
+	return (
+		<>
+			<TouchableOpacity
+				onPress={() => setIsPressed(!isPressed)}
+				className={`bg-white border p-4 border-gray-200 ${
+					isPressed && "border-b-0"
+				}`}
+			>
+				<View className="flex-row">
+					<View className="flex-1 pr-2">
+						<View>
+							<Text className="text-lg mb-1">{name}</Text>
+							<Text className="text-gray-400">{description}</Text>
+						</View>
+
+						<Text className="text-gray-400 mt-2">
+							<ReactCurrencyFormatter quantity={price} currency="AUD" />
+						</Text>
+					</View>
+					<View>
+						<Image
+							style={{
+								borderWidth: 1,
+								borderColor: "#F3F3F4",
+							}}
+							source={{ uri: urlFor(image).url() }}
+							className="h-20 w-20 bg-gray-300 p-4"
+						/>
+					</View>
+				</View>
+			</TouchableOpacity>
+
+			{isPressed && (
+				<View className="bg-white px-4">
+					<View className="flex-row items-center space-x-2 pb-3">
+						<TouchableOpacity>
+							<MinusCircleIcon
+								// color={item.length > 0 ? "#00CCBB" : "gray"}
+								size={40}
+							/>
+						</TouchableOpacity>
+						<Text>0</Text>
+						<TouchableOpacity>
+							<PlusCircleIcon size={40} />
+						</TouchableOpacity>
+					</View>
+				</View>
+			)}
+		</>
+	)
+}
+
+export default DishRow
