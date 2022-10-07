776d346d1a3c352b736ec4f0ffd19d6775c7dcbc
Rafael Vidal <rafael@oqea.com>
2022-10-07 19:02:14 +0800

    pass restaurant info to store via slice

--- store.js
+++ store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit"
 import cartReducer from "./features/cartSlice"
+import restaurantReducer from "./features/restaurantSlice"
 
 export const store = configureStore({
-	reducer: { cart: cartReducer },
+	reducer: { cart: cartReducer, restaurant: restaurantReducer },
 })
